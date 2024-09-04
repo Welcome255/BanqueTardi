cd19673a50f597605c82fe5bb300b35d80c12e72
Welcome225 <2394702@cegeplimoilou.ca>
2024-09-04 15:10:16 -0400

    Add project files.

---
+++ ContexteDb/BanqueTardiContexte.cs
@@ -0,0 +1,24 @@
+﻿using BanqueTardi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace BanqueTardi.ContexteDb
+{
+    public class BanqueTardiContexte : DbContext
+    {
+        public BanqueTardiContexte(DbContextOptions<BanqueTardiContexte> options) : base(options) { }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Compte> Compte { get; set; }
+        public DbSet<Operation> Operations { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Client>().ToTable(nameof(Client));
+            modelBuilder.Entity<Compte>().ToTable(nameof(Compte));
+            modelBuilder.Entity<Operation>().ToTable(nameof(Operation));
+        }
+    }
+}
